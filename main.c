369d5e053974a98603bd282fabefc7004cfdda6a
Van Huynh <vthuynh@cpp.edu>
2021-10-07 14:21:40 -0700

    deleted main's files and make outline for Loshu Magic Square code

---
+++ main.c
@@ -0,0 +1,8 @@
+/*
+Lo Shu Magic Square
+
+1. create main() that declare a empty 2D array, call generateSquare() and isMagicSquare() function
+2. implement generateSquare() function that generate square to be tested
+3. implement isMagicSquare() function that return "true" if the square is Lo Shu Magic Square and "false" if not
+4. 
+*/
