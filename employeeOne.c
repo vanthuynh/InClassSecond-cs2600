21358c3034d844ac41d970985bf0b46d8b3c2ece
Van Huynh <vthuynh@cpp.edu>
2021-10-07 10:47:26 -0700

    delete not related files and lecture comments

--- employeeOne.c
+++ employeeOne.c
@@ -4,15 +4,13 @@
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber){
     const PtrToConstEmployee endPtr = ptr + tableSize;  // ptr will stop traversing the table when it meets endPtr
 
-    for(; ptr < endPtr; ptr++)      // search until end of table
+    for(; ptr < endPtr; ptr++)
     {
-        if(ptr->number == targetNumber)     // check if it equals the Employee number
-            return (PtrToEmployee) ptr;     // return location of the number to callee.
+        if(ptr->number == targetNumber)
+            return (PtrToEmployee) ptr;
     }
-    return NULL;                    // this will only happen if no Employee number matches in loop above
+    return NULL; 
 }
-
-
 // Essentially the same functionality as above but comparing strings to check if equal
 PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char * targetName){
     const PtrToConstEmployee endPtr = ptr + tableSize;
--- employeeTable.c
+++ employeeTable.c
@@ -2,24 +2,12 @@
 #include <stdlib.h>
 #include "employee.h"
 
-Employee EmployeeTable[] =  // array of employee, no need to define the size since we already initialize with 5 employees
+Employee EmployeeTable[] =
 {
-    {1001l, "Daphne Borromeo",  "909-555-2134", 8.78},//'l' at the end inteprets the number as "long" data type
+    {1001l, "Daphne Borromeo",  "909-555-2134", 8.78},
     {1011l, "Tammy Franklin",   "213-555-1212", 4.50},
     {1140l, "Dorris Perl",      "310-555-1215", 7.80},
     {4011l, "Tony Bobcat",      "909-555-1235", 6.34},
     {5045l, "Brian Height",     "714-555-2749", 8.32},
 };
-
 const int EmployeeTableEntries = sizeof(EmployeeTable)/sizeof(EmployeeTable[0]);// 160/32 = 5 employees
-
-// Explain padding in gcc compiler. Each member in struct Employee = 8 bytes
-// Depends on Compiler used for Memory alignment. All members acan be accessed by incrementing equal distribution
-// void main() {
-//     printf("%d\n",sizeof(EmployeeTable));
-//     printf("%d\n",sizeof(EmployeeTable[0]));
-//     printf("%d\n",sizeof(long));
-//     printf("%d\n",sizeof(char*));   // I have Intel 64 bit processor --> 8 bytes for address
-//     printf("%d\n",sizeof(char*));   // Note this may change on your processor. RPI3--32 bit processor
-//     printf("%d\n",sizeof(double));
-// }
