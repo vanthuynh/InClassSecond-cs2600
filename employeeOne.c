2cefe8318d24a1addea70a390c80398650bd04e7
Van Huynh <vthuynh@cpp.edu>
2021-10-07 11:15:51 -0700

    finish implementing search for phone number and salary using multiple function

--- employeeOne.c
+++ employeeOne.c
@@ -22,3 +22,28 @@ PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *
     }
     return NULL;
 }
+
+// compare phone number to search for employee
+PtrToEmployee searchEmployeeByPhoneNumber(PtrToConstEmployee ptr, int tableSize, char* phoneToFind)
+{
+    const PtrToConstEmployee endPtr = ptr + tableSize;  // ptr will stop traversing the table when it meets endPtr
+
+    for(; ptr < endPtr; ptr++)
+    {
+        if(strcmp(ptr->phone,phoneToFind) == 0)
+            return (PtrToEmployee) ptr;
+    }
+    return NULL; 
+}
+
+// compare salary to search for employee
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, double salaryToFind)
+{
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    for(; ptr < endPtr; ptr++)
+    {
+        if(ptr->salary == salaryToFind)
+            return (PtrToEmployee) ptr;
+    }
+    return NULL;
+}
